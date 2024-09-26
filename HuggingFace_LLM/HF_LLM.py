a292cf1985263c174627e321a2e5b1696d20b3e9
danielfirouzabadi <dany.firou@gmail.com>
2024-09-26 13:28:48 +0200

    Update HF_LLM.py

--- HuggingFace_LLM/HF_LLM.py
+++ HuggingFace_LLM/HF_LLM.py
@@ -1,6 +1,7 @@
-
-
 from datasets import load_dataset
+import pandas as pd
+import matplotlib.pyplot as plt
+
 emotions = load_dataset("emotion")
 emotions
 print(emotions)
@@ -12,3 +13,20 @@ print(len(train_ds)) #length of train dataset
 print(train_ds[0]) #accesss a single exmaple by its index
 print(train_ds.column_names) #access column names
 print(train_ds.features) #access features
+print(train_ds[:5]) #access 5 rows
+print(train_ds["text"][:5]) #get the full column by name
+
+emotions.set_format(type="pandas") #convert to pandas dataframe for visualization 
+df=emotions["train"][:]
+print(df.head())
+
+def label_int2str(row):
+    return emotions["train"].features["label"].int2str(row) #use int2str to change label integers to add coloumn w/ corresponding labelnames
+
+df["label_name"] = df["label"].apply(label_int2str)
+print(df.head())
+
+#using Matplotlib for visualizing class distribution
+df["label_name"].value_counts(ascending=True).plot.barh()
+plt.title("Frequency of Classes")
+plt.show()
